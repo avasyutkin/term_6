94c30d4c94fcceede6fd0ea41c61bb797a6df38a
Alexander <43503189+avasyutkin@users.noreply.github.com>
2021-02-17 18:22:12 +0300

    вывод в виде строки, расширенный алфавит

--- АТБАШ/code.py
+++ АТБАШ/code.py
@@ -1,25 +1,28 @@
 alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
 
+#alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', '.', ',', '-', '!', '?', ':', ';', '—', ' ', '(', ')']
+
 def encryption(message, encrypted_message):
     for symbol in message:
-        symbol = 31 - alphabet.index(symbol)
-        encrypted_message.append(alphabet[symbol])
+        symbol = 31 - alphabet.index(symbol)   # для расширенного алфавита - 76
+        encrypted_message = encrypted_message + str(alphabet[symbol])
     return encrypted_message
 
 def decryption(encrypted_message, decrypted_message):
     for symbol in encrypted_message:
-        symbol = 31 - alphabet.index(symbol)
-        decrypted_message.append(alphabet[symbol])
+        symbol = 31 - alphabet.index(symbol)   # для расширенного алфавита - 76
+        decrypted_message = decrypted_message + str(alphabet[symbol])
     return decrypted_message
 
 
 message = input("Введите фразу: ")
 
-encrypted_message = []
-encryption(message, encrypted_message)
+encrypted_message = ''
+encrypted_message = encryption(message, encrypted_message)
+
 print(encrypted_message)
 
-decrypted_message = []
-decryption(encrypted_message, decrypted_message)
-print(decrypted_message)
+decrypted_message = ''
+decrypted_message = decryption(encrypted_message, decrypted_message)
 
+print(decrypted_message)
--- Квадрат Полибия/code.py
+++ Квадрат Полибия/code.py
@@ -5,6 +5,18 @@ alphabet = [['а', 'б', 'в', 'г', 'д', 'е'],
             ['ш', 'щ', 'ъ', 'ы', 'ь', 'э'],
             ['ю', 'я']]
 
+"""
+alphabet = [['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з'],
+            ['и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р'],
+            ['с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'],
+            ['ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В'],
+            ['Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К'],
+            ['Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У'],
+            ['Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь'],
+            ['Э', 'Ю', 'Я', '.', ',', '-', '!', '?', ':'],
+            [';', '—', ' ', '(', ')']]
+"""
+
 def encryption(message, encrypted_message):
     for symbol in message:
         for i in range(len(alphabet)):
@@ -18,16 +30,18 @@ def decryption(encrypted_message, decrypted_message):
         for i in range(len(alphabet)):
             for j in range(len(alphabet[i])):
                 if str(i+1)+str(j+1) == symbol:
-                    decrypted_message.append(alphabet[i][j])
+                    decrypted_message = decrypted_message + str(alphabet[i][j])
     return decrypted_message
 
 
-message = input("Введите сообщение: ")
+message = input("Введите фразу: ")
 
 encrypted_message = []
-encryption(message, encrypted_message)
+encrypted_message = encryption(message, encrypted_message)
+
 print(encrypted_message)
 
-decrypted_message = []
-decryption(encrypted_message, decrypted_message)
+decrypted_message = ''
+decrypted_message = decryption(encrypted_message, decrypted_message)
+
 print(decrypted_message)
